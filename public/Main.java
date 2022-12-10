a9bfb24ead05a7a319476bbb9bee4a9d197d0595
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 17:45:18 +0200

    Update Main.java

--- public/Main.java
+++ public/Main.java
@@ -5,5 +5,7 @@ package package02;
 
 public class Main {
      
-   public void method01() { System.out.println("...transmitting form class Main"); }
+   public void method01() { 
+        System.out.println("...transmitting form class Main"); 
+   }
 } // end of public class Main
