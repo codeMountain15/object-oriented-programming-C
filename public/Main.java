80954098d7050a0a8ce91837dda842c40e7e99e6
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 17:36:42 +0200

    Create Main.java

---
+++ public/Main.java
@@ -0,0 +1,9 @@
+// Main.java
+//
+
+package package02;
+
+public class Main {
+     
+   public void method01() { System.out.println("...transmitting form class Main"); }
+} // end of public class Main
