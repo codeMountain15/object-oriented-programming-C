85d9e0c02e85d5d37543b166f4159cbc5fafc4d7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 21:53:43 +0200

    Create Mpee01.java

---
+++ source_code/protected/Mpee01.java
@@ -0,0 +1,47 @@
+// Mpee01.java
+//
+
+package stani;
+//import stani.Mpee02;
+
+class Mpee01 extends Mpee02 {
+    
+    // constructor
+    Mpee01(){
+        System.out.println("This is Mpee01 (child class) constructor"); 
+    }
+    
+    void method02() { 
+        System.out.println("...transmitting 'MPEEE MPEEEEE' from child (Mpee01)"); 
+   }
+    
+    // method
+    public static void main(String[] args){
+        System.out.println("This is class Mpee01");
+        System.out.println();
+        
+        // a parent object
+        Mpee02 obj_a = new Mpee02();
+        obj_a.method01();
+        System.out.println();
+        
+        // a child object
+        Mpee01 obj_b = new Mpee01();
+        obj_b.method01();
+        System.out.println();
+        
+        // and a more complicated example:
+        // a child object referencing to parent
+        Mpee02 obj_c = new Mpee01();
+        obj_c.method01(); // the Mpee2 (parent) method call
+        System.out.println();
+        
+        /* // -----> error!
+        System.out.println("Calling a Mpee01 object method:");
+        Mpee01 obj_d = new Mpee02();
+        obj_d.method01();
+        System.out.println();
+        */
+        
+    } // end of main()
+} // end of class Mpee01
