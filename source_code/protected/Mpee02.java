c2297cd2ec75fdc797ae99fd872643e577434bd7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 21:54:18 +0200

    Create Mpee02.java

---
+++ source_code/protected/Mpee02.java
@@ -0,0 +1,18 @@
+// Mpee02.java
+// protected member
+
+package stani;
+
+// protected class Mpee02{ // ---> error!
+class Mpee02{
+   
+    // constructor
+    Mpee02(){
+        System.out.println("This is Mpee02 (parent class) constructor"); 
+    }
+    
+    // method
+    protected void method01() { 
+        System.out.println("...transmitting 'MPEEE MPEEEEE'"); 
+   }
+} // end of class Mpee02
