2b64f7c6ee859fbae30c3adccc99d50b9feadd89
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 17:37:43 +0200

    Create Lesson16.java

---
+++ public/Lesson16.java
@@ -0,0 +1,16 @@
+// Lesson16.java
+//
+
+package package01;
+
+import package02.Main;
+
+public class Lesson16 {
+    
+    public static void main(String[] args){
+        System.out.println("This is class Lesson16");
+        
+        Main obj = new Main();
+        obj.method01();
+    } // end of main()
+} // end of public class Lesson16
