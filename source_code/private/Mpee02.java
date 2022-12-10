96ffbd0592bb53e38c7012caab6c9e58c9d434d8
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-10 22:41:50 +0200

    Create Mpee02.java

---
+++ source_code/private/Mpee02.java
@@ -0,0 +1,13 @@
+// Mpee02.java
+// private member
+
+package stani;
+
+// protected class Mpee02{ // ---> error!
+public class Mpee02{
+    
+    // method
+    private void method01() { 
+        System.out.println("...transmitting 'MPEEE MPEEEEE'"); 
+   }
+} // end of class Mpee02
