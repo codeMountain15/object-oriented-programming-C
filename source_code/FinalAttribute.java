6f1048a3a1eba7535975545ffbdf17cd95312087
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-11 21:56:31 +0200

    Create FinalAttribute.java

---
+++ source_code/FinalAttribute.java
@@ -0,0 +1,16 @@
+// FinalAttribute.java
+// final attribute
+// is a const attribute
+
+package Department;
+
+class FinalAttribute{
+        
+    public static void main(String[] args) { 
+        final int constantNum = 100;
+        
+        System.out.println(constantNum);
+        // constantNum++; // eror!
+        
+   }
+} // end of class FinalAttribute
