bffe576ba5c4c1027c1f1de8dba05d585b45da89
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-11 17:44:53 +0200

    Create Super01.java

---
+++ source_code/Super01.java
@@ -0,0 +1,34 @@
+// Super01.java
+// overriden method
+
+package xeimadio;
+
+class Animal{
+    void speak() {
+        System.out.println("This is Animal speaking!");
+    };
+}
+
+class Ox extends Animal{
+    // method override
+    void speak() {
+        System.out.println("This is Dog speaking!");
+    };
+    
+    // method using super()
+    void superSpeak(){
+        super.speak();
+    }
+}
+
+
+class Super01 extends Ox{
+    
+    public static void main(String[] args) { 
+        Super01 obj01 = new Super01();
+        obj01.speak();
+        obj01.superSpeak();
+
+        
+   }
+} // end of class Super01
